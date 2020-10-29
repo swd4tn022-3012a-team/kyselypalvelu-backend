dbcb696e6b180076c2b9923d0ce01659e011ef26
Oskari Holopainen <oskariholopa@gmail.com>
2020-10-29 08:56:51 +0200

    Initial commit

---
+++ src/test/java/com/example/kyselypalvelu/KyselypalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kyselypalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselypalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
