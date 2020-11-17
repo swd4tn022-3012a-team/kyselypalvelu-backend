6acf5413f40baa6d1901985fbd92c38ade879d22
Oskari Holopainen <oskariholopa@gmail.com>
2020-11-17 15:04:16 +0200

    Add HH example questionnaire

--- src/main/java/com/example/kyselypalvelu/KyselypalveluApplication.java
+++ src/main/java/com/example/kyselypalvelu/KyselypalveluApplication.java
@@ -1,5 +1,7 @@
 package com.example.kyselypalvelu;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,22 +23,30 @@ public class KyselypalveluApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner initialQuestionsAndQuestionnaires(QuestionRepository questions, QuestionnaireRepository questionnaires) {
+	public CommandLineRunner initialQuestionsAndQuestionnaires(QuestionRepository questionRepository, QuestionnaireRepository questionnaireRepository) {
 		return (args) -> {
 			log.info("insert a test question and questionnaire");
 			//Luodaan kysely, jolle annetaan parametreina otsikko ja kuvaus
-			Questionnaire questionnaire = new Questionnaire("Henkilökysely", "Tämä on henkilötietokysely");
+			Questionnaire questionnaire = new Questionnaire("HH-esimerkkikysely", "Anna palautetta opintojaksosta");
 			
 			//Tallennetaan kysely kantaan ja tulostetaan siitä tietoja
-			log.info("FIRST QUESTIONNAIRE ID:" + questionnaires.save(questionnaire).getQuestionnaireId().toString());
+			log.info("FIRST QUESTIONNAIRE ID:" + questionnaireRepository.save(questionnaire).getQuestionnaireId().toString());
 			
-			//Luodaan kaksi uutta kysymystä, jolle parametrina kysymyksen teksti  ja kysely, johon kysymys liittyy
-			Question first = new Question("Mikä on nimesi?", questionnaire);
-			Question second = new Question("Kuinka vanha olet?", questionnaire);
+			//Luodaan HH esimerkkikysely
+			ArrayList<Question> questions = new ArrayList<Question>();
+			questions.add(new Question("Tukivatko kurssin opiskelutavat oppimistasi?", questionnaire));
+			questions.add(new Question("Missä olisi mielestäsi parannettavaa?", questionnaire));
+			questions.add(new Question("Tunsitko saavasi apua kun sitä tarvitsit?", questionnaire));
+			questions.add(new Question("Mikä oli sinusta merkittävintä mitä opit kurssin aikana?", questionnaire));
+			questions.add(new Question("Oliko kurssi sinusta merkittävä?", questionnaire));
+			questions.add(new Question("Miten itse kehittäisit kurssia?", questionnaire));
+			questions.add(new Question("Missä kurssi sinusta onnistui?", questionnaire));
+			questions.add(new Question("Minkä arvosanan annat opetuksesta? (1-5)", questionnaire));
+			questions.add(new Question("Minkä arvosanan antaisit kurssin oppimateriaaleille? (1-5)", questionnaire));
+			questions.add(new Question("Minkä arvosanan annat kokonaisuudessaan kurssille? (1-5)", questionnaire));
 			
 			//Tallennetaan kysymykset kantaan
-			questions.save(first);
-			questions.save(second);
+			questionRepository.saveAll(questions);
 		};
 	}
 
