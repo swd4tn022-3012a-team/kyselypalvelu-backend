c6318f159900fa8946032ca48ff764669d1693c9
Oskari Holopainen <oskariholopa@gmail.com>
2020-11-19 09:18:31 +0200

    Remove unused imports

--- src/main/java/com/example/kyselypalvelu/webcontroller/AnswerController.java
+++ src/main/java/com/example/kyselypalvelu/webcontroller/AnswerController.java
@@ -1,8 +1,6 @@
 package com.example.kyselypalvelu.webcontroller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import com.example.kyselypalvelu.domain.Answer;
 import com.example.kyselypalvelu.domain.AnswerRepository;
-import com.example.kyselypalvelu.domain.Questionnaire;
 
 @Controller
 public class AnswerController {
